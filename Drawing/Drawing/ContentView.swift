35a7e6bffdf3c29d469e0c92489fb613c5dfe3ce
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2022-12-30 11:12:52 -0300

    Studying Core Animation and Metal Frameworks

--- Drawing/Drawing/ContentView.swift
+++ Drawing/Drawing/ContentView.swift
@@ -7,42 +7,15 @@
 
 import SwiftUI
 
-struct PathView: View{
-    var body: some View{
-        Path{ path in
-            path.move(to: CGPoint(x: 200, y: 100))
-            
-            path.addLine(to: CGPoint(x: 100, y: 300))
-            path.addLine(to: CGPoint(x: 300, y: 300))
-            path.addLine(to: CGPoint(x: 200, y: 100))
-        }
-        .stroke(.blue, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
-    }
-}
-
-struct Triangle: Shape{
-    func path(in rect: CGRect) -> Path {
-        var path = Path()
-        
-        path.move(to: CGPoint(x: rect.midX, y: rect.minY))  // Stating Position
-        
-        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
-        
-        return path
-    }
-}
-
 struct Arc: InsettableShape{
     var startAngle: Angle
     var endAngle: Angle
     var clockwise: Bool
-    
     var insetAmount = 0.0
     
     func path(in rect: CGRect) -> Path {
         let rotationAdjustment = Angle.degrees(90)
+        
         let modifiedStart = startAngle - rotationAdjustment
         let modifiedEnd = endAngle - rotationAdjustment
         
@@ -53,36 +26,99 @@ struct Arc: InsettableShape{
         return path
     }
     
-    func inset(by amount: CGFloat) -> some InsettableShape {
+    func inset(by amount: CGFloat) -> some InsettableShape{
         var arc = self
         arc.insetAmount += amount
-        
         return arc
     }
 }
 
+struct Flower: Shape{
+    var petalOffset = -20.0
+    var petalWidth = 100.0
+    
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        for number in stride(from: 0, to: Double.pi * 2, by: Double.pi / 8){// 180° == pi in radians, 360° == pi * 2 in radians
+            let rotation = CGAffineTransform(rotationAngle: number)
+            let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
+            
+            let originalPetal = Path(ellipseIn: CGRect(x: petalOffset, y: 0, width: petalWidth, height: rect.width / 2))
+            
+            let rotatedPetal = originalPetal.applying(position)
+            
+            path.addPath(rotatedPetal)
+        }
+        
+        return path
+    }
+}
+
+struct ColorCyclingCircle: View{
+    var amount = 0.0
+    var steps = 100
+    
+    var body: some View{
+        ZStack{
+            ForEach(0..<steps){ value in
+                Circle()
+                    .inset(by: Double(value))  // Adjusting
+                    .strokeBorder(
+                        LinearGradient(colors: [
+                            color(for: value, brigthness: 1),
+                            color(for: value, brigthness: 0.7)
+                        ], startPoint: .top, endPoint: .bottom)
+                    )
+            }
+        }
+        .drawingGroup()  // This tells SwiftUI it should render the contents of the view into an off-screen image before putting it back onto the screen as a single rendered output, which is significantly faster. Behind the scenes this is powered by Metal, which is Apple’s framework for working directly with the GPU for extremely fast graphics.
+    }
+    
+    func color(for value: Int, brigthness: Double) -> Color{
+        var targetHue = Double(value) / Double(steps) + amount
+        
+        if targetHue > 1{
+            targetHue -= 1
+        }
+        
+        return Color(hue: targetHue, saturation: 1, brightness: brigthness)
+    }
+}
+
 struct ContentView: View {
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+    @State private var paddingArround = 80.0
+    
+    @State private var colorCycle = 0.0
+    
     var body: some View {
-        /*
-         VStack{
-             PathView()
-             
-             Triangle()
-                 .stroke(.red, style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
-                 .frame(width: 300, height: 300)
-             
-             Arc(startAngle: .degrees(0), endAngle: .degrees(110), clockwise: true)
-                 .stroke(.blue, lineWidth: 10)
-                 .frame(width: 300, height: 300)
-             
-         }
-         */
-        
-        Arc(startAngle: .degrees(-90), endAngle: .degrees(90), clockwise: true)
-            .strokeBorder(.blue, lineWidth: 40)
-         
-        
-        
+        VStack{
+            ColorCyclingCircle(amount: colorCycle)
+            
+            Slider(value: $colorCycle)  // Automatically 0...1
+                .padding([.horizontal, .bottom])
+            
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .fill(.red, style: FillStyle(eoFill: true))
+                .padding(paddingArround)
+            
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+            
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding([.horizontal, .bottom])
+            
+            Text("Padding Around Flower")
+            Slider(value: $paddingArround, in: 80...100, step: 1.0)
+                .padding(.horizontal)
+            
+            
+            
+        }
     }
 }
 
