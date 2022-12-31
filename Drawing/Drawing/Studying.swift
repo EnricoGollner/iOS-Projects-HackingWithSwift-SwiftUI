6f5357f2aaaab1a967d70bafa90b4f8930a48aad
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2022-12-31 12:52:32 -0300

    More studying

---
+++ Drawing/Drawing/Studying.swift
@@ -0,0 +1,20 @@
+//
+//  Studying.swift
+//  Drawing
+//
+//  Created by Enrico Sousa Gollner on 31/12/22.
+//
+
+import SwiftUI
+
+struct Studying: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Studying_Previews: PreviewProvider {
+    static var previews: some View {
+        Studying()
+    }
+}
---
+++ Drawing/Drawing/Studying2.swift
@@ -0,0 +1,20 @@
+//
+//  Studying2.swift
+//  Drawing
+//
+//  Created by Enrico Sousa Gollner on 31/12/22.
+//
+
+import SwiftUI
+
+struct Studying2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Studying2_Previews: PreviewProvider {
+    static var previews: some View {
+        Studying2()
+    }
+}
