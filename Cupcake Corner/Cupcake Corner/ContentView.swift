04a9c01ff8f2edd685e141115e43736282ea975e
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2023-01-08 03:56:39 -0300

    Observations

--- Cupcake Corner/Cupcake Corner/ContentView.swift
+++ Cupcake Corner/Cupcake Corner/ContentView.swift
@@ -6,50 +6,9 @@
 
 import SwiftUI
 
-// The API: https://itunes.apple.com/search?term=pink+floyd&entity=song
-
-struct Response: Codable{
-    var results: [Result]
-}
-
-struct Result: Codable{
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 struct ContentView: View {
-    @State private var results = [Result]()
-    
     var body: some View {
-        List(results, id: \.trackId){ item in
-            VStack(alignment: .leading){
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
-            }
-        }
-        .task {
-            await loadData()
-        }
-    }
-    
-    func loadData() async{
-        guard let url = URL(string: "https://itunes.apple.com/search?term=pink+floyd&entity=song") else{
-            print("Invalid Url")
-            return
-        }
-        
-        do{
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data){
-                results = decodedResponse.results
-            }
-            
-        } catch{
-            print("Invalid Data")
-        }
+        Text("Hello, world!")
     }
 }
 
