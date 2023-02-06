5b77ffd6793e8852f64834287dfd043b10820d19
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2023-02-06 17:19:26 -0300

    Core Data Version Start

---
+++ Milestone10to12CoreData/Milestone10to12CoreData/Milestone10to12CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Milestone10to12CoreDataApp.swift
+//  Milestone10to12CoreData
+//
+//  Created by Enrico Sousa Gollner on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Milestone10to12CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
