d1b2a8b3440572ab3eaefdcfb721f935a421796c
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2023-02-14 23:38:15 -0300

    Organizing and Wrapping a UIViewController in a SwiftUI view

--- InstaFilter/InstaFilter/ContentView.swift
+++ InstaFilter/InstaFilter/ContentView.swift
@@ -5,54 +5,28 @@
 //  Created by Enrico Sousa Gollner on 11/02/23.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
     @State private var image: Image?
-    
-    @State private var itensityFilter = 0.0
+    @State private var showingImagePicker = false
     
     var body: some View{
         VStack{
             image?
                 .resizable()
                 .scaledToFit()
+            
+            Button("Select Image"){
+                showingImagePicker = true
+            }
         }
-        .onAppear(perform: loadImage)
+        .sheet(isPresented: $showingImagePicker) {
+            ImagePicker()
+        }
     }
     
-    func loadImage(){
-        guard let inputImage = UIImage(named: "Example") else { return }  // UIImage
-        let beginImage = CIImage(image: inputImage)  // Converting UIImage to CIImage
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.sepiaTone()
-        currentFilter.inputImage = beginImage
-        
-        let amount = 1.0
-        let inputKeys = currentFilter.inputKeys
-        
-        if inputKeys.contains(kCIInputIntensityKey){
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
-        }
-        
-        if inputKeys.contains(kCIInputRadiusKey){
-            currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey)
-        }
-        
-        if inputKeys.contains(kCIInputScaleKey){
-            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
-        }
-        
-        guard let outputImage = currentFilter.outputImage else { return }  // CIImage
-        
-        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent){  // Converting CIImage to CGImage
-            let uiImage = UIImage(cgImage: cgImage)
-            image = Image(uiImage: uiImage)
-        }
-    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
