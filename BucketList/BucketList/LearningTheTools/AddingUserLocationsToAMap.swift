60e65a3d2750fdc31600048f0df140a6f4e9e0db
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2023-02-23 00:50:29 -0300

    Day 70 - Adding user locations to a map

--- BucketList/BucketList/LearningTheTools/AddingUserLocationsToAMap.swift
+++ BucketList/BucketList/LearningTheTools/AddingUserLocationsToAMap.swift
@@ -9,8 +9,43 @@ import MapKit
 import SwiftUI
 
 struct AddingUserLocationsToAMap: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
+    
+    @State private var locations = [Location]()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Map(coordinateRegion: $mapRegion, annotationItems: locations){ location in
+                MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
+            }
+            .ignoresSafeArea()
+            
+            Circle()
+                .fill(.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            
+            VStack{
+                Spacer()
+                HStack{
+                    Spacer()
+                    
+                    Button{
+                        let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+                        
+                        locations.append(newLocation)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
+                }
+            }
+        }
     }
 }
 
