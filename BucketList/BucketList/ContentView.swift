31948e031bcb6942b8d8f117a24c892892f3c7fd
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2023-02-23 00:32:36 -0300

    Adding user location to a map

--- BucketList/BucketList/ContentView.swift
+++ BucketList/BucketList/ContentView.swift
@@ -5,69 +5,47 @@
 //  Created by Enrico Sousa Gollner on 19/02/23.
 //
 
-import LocalAuthentication
 import MapKit
 import SwiftUI
 
-struct LocationsL: Identifiable{
-    let id = UUID()
-    let name: String
-    let coordinator: CLLocationCoordinate2D
-}
-
 struct ContentView: View{
-    @State private var isUnlocked = false
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
     
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
-    
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
+    @State private var locations = [Location]()
     
     var body: some View {
-        NavigationStack{
-            VStack{
-                if isUnlocked{
-                    VStack{
-                        Map(coordinateRegion: $mapRegion, annotationItems: locations){ location in
-                            MapAnnotation(coordinate: location.coordinate){
-                                Circle()
-                                    .stroke(.red, lineWidth: 2)
-                                    .frame(width: 30, height: 30)
-                            }
-                        }
-                    }
-                    .navigationTitle("London Explorer")
-                } else {
-                    VStack(spacing: 4){
-                        Text("App Locked")
-                        
-                        Button("Try it again", action: authenticate)
-                    }
-                    
-                }
+        ZStack{
+            Map(coordinateRegion: $mapRegion, annotationItems: locations){ location in
+                MapMarker(coordinate: CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude))
             }
-            .onAppear(perform: authenticate)
-        }
-    }
-    
-    func authenticate(){
-        let context = LAContext()
-        var error: NSError?
-        
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
-            let reason = "We need you to unlock the data and the app"
+            .ignoresSafeArea()
             
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason){ success, authenticationError in
-                if success{
-                    isUnlocked = true
-                } else{
-                    print("There's a problem")
+            Circle()
+                .fill(.blue)
+                .opacity(0.3)
+                .frame(width: 32, height: 32)
+            
+            VStack{
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button{
+                        let newLocation = Location(id: UUID(), name: "New location", description: "", latitude: mapRegion.center.latitude, longitude: mapRegion.center.longitude)
+                        
+                        locations.append(newLocation)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                    .padding()
+                    .background(.black.opacity(0.75))
+                    .foregroundColor(.white)
+                    .font(.title)
+                    .clipShape(Circle())
+                    .padding(.trailing)
                 }
             }
-        } else {
-            print("User's device have not biometrics or haven't enrolled it")
         }
     }
 }
---
+++ BucketList/BucketList/LearningTheTools/AddingUserLocationsToAMap.swift
@@ -0,0 +1,21 @@
+//
+//  AddingUserLocationsToAMap.swift
+//  BucketList
+//
+//  Created by Enrico Sousa Gollner on 22/02/23.
+//
+
+import MapKit
+import SwiftUI
+
+struct AddingUserLocationsToAMap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddingUserLocationsToAMap_Previews: PreviewProvider {
+    static var previews: some View {
+        AddingUserLocationsToAMap()
+    }
+}
