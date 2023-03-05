16d5c71a4d4f5090f09fa0eb60c9991b70970072
Enrico Gollner <102383144+EnricoGollner@users.noreply.github.com>
2023-03-05 01:22:09 -0300

    Organizing

--- MapExplorer/MapExplorer/Location.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Location.swift
-//  MapExplorer
-//
-//  Created by Enrico Sousa Gollner on 24/02/23.
-//
-
-import Foundation
--- MapExplorerV2.0/MapExplorerV2.0/Location.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Location.swift
-//  MapExplorerV2.0
-//
-//  Created by Enrico Sousa Gollner on 03/03/23.
-//
-
-import Foundation
